5a5f9f2c6be6dbb9d8571a5252f65c13141b07d4
ahmedmumdouh <ahmedmumdouh@gmail.com>
2021-05-05 09:49:36 +0200

    add deploy config

---
+++ server.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+app.use(express.static(__dirname + '/dist/my-first-project'));
+app.get('/*', function(req,res) {
+  res.sendFile(path.join(__dirname+
+    '/dist/my-first-project/index.html'));});
