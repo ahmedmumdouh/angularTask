3ec2dedfb704a9d3c652a7dd920770947dae99b5
ahmedmumdouh <ahmedmumdouh@gmail.com>
2021-05-05 10:03:57 +0200

    add deploy config

--- server.js
+++ server.js
@@ -5,3 +5,15 @@ app.use(express.static(__dirname + '/dist/my-first-project'));
 app.get('/*', function(req,res) {
   res.sendFile(path.join(__dirname+
     '/dist/my-first-project/index.html'));});
+
+
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('db.json'); // <== Will be created later
+const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 8080; // <== You can change the port
+
+server.use(middlewares);
+server.use(router);
+
+server.listen(port);
