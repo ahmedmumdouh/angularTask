91df8198aaafb74de88f68eee852b7375ff0198e
ahmedmumdouh <ahmedmumdouh@gmail.com>
2021-05-05 11:25:50 +0200

    add deploy config

--- server.js
+++ server.js
@@ -1,19 +1,36 @@
+// const express = require('express');
+// const path = require('path');
+// const app = express();
+// app.use(express.static(__dirname + '/dist/my-first-project'));
+// app.get('/*', function(req,res) {
+//   res.sendFile(path.join(__dirname+
+//     '/dist/my-first-project/index.html'));});
+
+
+// // const jsonServer = require('json-server');
+// // const server = jsonServer.create();
+// // const router = jsonServer.router('db.json'); // <== Will be created later
+// // const middlewares = jsonServer.defaults();
+// const port = process.env.PORT || 8080; // <== You can change the port
+
+// // server.use(middlewares);
+// // server.use(router);
+
+// app.listen(port);
+
+//Install express server
 const express = require('express');
 const path = require('path');
+
 const app = express();
+
+// Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/my-first-project'));
+
 app.get('/*', function(req,res) {
-  res.sendFile(path.join(__dirname+
-    '/dist/my-first-project/index.html'));});
+    
+res.sendFile(path.join(__dirname+'/dist/my-first-project/index.html'));
+});
 
-
-const jsonServer = require('json-server');
-const server = jsonServer.create();
-const router = jsonServer.router('db.json'); // <== Will be created later
-const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 8080; // <== You can change the port
-
-server.use(middlewares);
-server.use(router);
-
-server.listen(port);
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -8,7 +8,7 @@ import {User} from '../models/user';
 })
 export class UsersService {
 
-  readonly baseURL:string = "http://localhost:8080/users/";
+  readonly baseURL:string = "http://localhost:3000/users/";
 
   myObservable:Observable<number>
   constructor(private myclient:HttpClient) {
